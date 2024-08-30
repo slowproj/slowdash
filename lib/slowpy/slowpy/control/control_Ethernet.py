6fa4a98074ba499ffed382b8a98ef71faf7919ee
Sanshiro Enomoto <sanshiro@uw.edu>
2024-08-29 18:56:47 -0700

    SlowPy/Control: added Telnet node

--- lib/slowpy/slowpy/control/control_Ethernet.py
+++ lib/slowpy/slowpy/control/control_Ethernet.py
@@ -1,6 +1,6 @@
 
 from slowpy.control import ControlNode
-import socket, selectors
+import socket, selectors, time
 
 
 class EthernetNode(ControlNode):
@@ -36,25 +36,41 @@ class EthernetNode(ControlNode):
 
     
     def get(self):
+        return self.do_get_line()
+
+
+    ## methods ##    
+    def do_get_chunk(self, timeout=1.0):
         if self.socket is None:
             return None
+    
+        events = self.selectors.select(timeout=timeout)
+        for key, mask in events:
+            if key.fileobj == self.socket and mask != 0:
+                return self.socket.recv(1024*1024).decode('utf-8', errors='ignore')
+        else:
+            return ''
+
+
+    def do_get_line(self, timeout=None):
+        if self.socket is None:
+            return None
+
+        if timeout is not None:
+            wait_until = time.time() + timeout
+        else:
+            wait_until = None
         
         line = ''
-        while True:
+        while not self.is_stop_requested():
             if len(self.socket_buffer) == 0:
-                events = self.selectors.select(timeout=0.1)
-                for key, mask in events:
-                    if key.fileobj == self.socket and mask != 0:
-                        break
-                else:
-                    if self.is_stop_requested():
-                        break
-                    else:
-                        continue
-                    
-                self.socket_buffer = self.socket.recv(1024).decode('utf-8', errors='ignore')
-                if len(self.socket_buffer) == 0:
+                self.socket_buffer = self.do_get_chunk(timeout=0.1)
+                
+            if len(self.socket_buffer) == 0:
+                if wait_until is not None and time.time() > wait_until:
                     break
+                else:
+                    continue
 
             for k in range(len(self.socket_buffer)):
                 ch = self.socket_buffer[k]
@@ -69,32 +85,21 @@ class EthernetNode(ControlNode):
         return line
 
 
-    ## methods ##
     def do_flush_input(self):
-        if self.socket is None:
-            return
-        
-        self.socket_buffer = ''
         while not self.is_stop_requested():
-            events = self.selectors.select(timeout=0.01)
-            for key, mask in events:
-                if key.fileobj == self.socket and mask != 0:
-                    print(f"dumping [{self.socket.recv(1024).decode('utf-8', errors='ignore').strip()}]")
-                    break
-            else:
+            text = self.do_get_chunk(timeout=0.01)
+            if len(text) == 0:
                 break
+            #print(f"dumping [{text.strip()}]")
     
 
     ## child nodes ##
     def scpi(self, base_name, **kwargs):
-        try:
-            self.scpi_nodes.keys()
-        except:
-            self.scpi_nodes = {}
-        if base_name not in self.scpi_nodes:
-            self.scpi_nodes[base_name] = ScpiNode(self, base_name, **kwargs)
-            
-        return self.scpi_nodes[base_name]
+        return ScpiNode(self, base_name, **kwargs)
+    
+
+    def telnet(self, command, **kwargs):
+        return TelnetNode(self, command, **kwargs)
     
 
     @classmethod
@@ -118,11 +123,12 @@ class EthernetNode(ControlNode):
     
     
 class ScpiNode(ControlNode):
-    def __init__(self, connection, base_name, set_format=None, sync=True):
+    def __init__(self, connection, base_name, set_format=None, sync=True, timeout=10):
         self.connection = connection
         self.base_name = base_name
         self.set_format = set_format
         self.sync = sync
+        self.timeout = timeout
         
     
     def set(self, value=None):
@@ -140,16 +146,62 @@ class ScpiNode(ControlNode):
         if self.sync:
             self.connection.get()
             
-
     
     def get(self):
         if self.base_name[-1] == '?':
             self.connection.set('%s' % self.base_name)
         else:
             self.connection.set('%s?' % self.base_name)
-        return self.connection.get()
+        return self.connection.do_get_line(timeout=timeout)
 
 
+    
+class TelnetNode(ControlNode):
+    def __init__(self, connection, command, prompt='>', timeout=10, has_echo=True):
+        self.connection = connection
+        self.command = command
+        self.prompt = prompt
+        self.timeout = timeout
+        self.has_echo = has_echo
+        
+    
+    def set(self, value=None):
+        if value is None:
+            cmd = self.command
+        else:
+            cmd = '%s %s' % (self.command, str(value))
+            
+        self.connection.do_flush_input()
+        self.connection.set(cmd)
+        self.do_get_lines_to_prompt()
+
+    
+    def get(self):
+        self.connection.do_flush_input()
+        self.connection.set(self.command)
+        lines = self.do_get_lines_to_prompt()
+        
+        if self.has_echo and len(lines) > 0 and lines[0] == self.command:
+            # TODO: check this is really the echo string
+            lines = lines[1:]
+
+        return '\n'.join(lines)
+            
+        
+    def do_get_lines_to_prompt(self):
+        lines = []
+        wait_until = time.time() + (self.timeout or 10)
+        
+        while not self.is_stop_requested() and time.time() < wait_until:
+            line = self.connection.do_get_line(timeout=0.1)
+            if len(line) == 0:
+                continue
+            if line.startswith(self.prompt):
+                break
+            lines.append(line.strip())
+
+        return lines
+        
     
 def export():
     return [ EthernetNode ]
