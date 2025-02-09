622753507e07a738fd321532d31c97cc8c913710
Sanshiro Enomoto <sanshiro@uw.edu>
2025-02-08 16:58:36 -0800

    setup-venv: check PostgreSQL libpq installation before pip install psycopg2

--- utils/slowdash-setup-venv.sh
+++ utils/slowdash-setup-venv.sh
@@ -11,7 +11,10 @@ source venv/bin/activate
 pip install uvicorn pyyaml psutil bcrypt requests 
 
 # DB interface packages
-pip install psycopg2 influxdb-client redis pymongo couchdb
+if command -v pg_config &> /dev/null; then
+    pip install psycopg2
+fi
+pip install influxdb-client redis pymongo couchdb
 
 # Packages that user scripts might use
 pip install numpy matplotlib pillow pyserial pyvisa
