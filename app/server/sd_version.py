d2d7473afe70a66557812cdd8d678de9a18c8b8e
Sanshiro Enomoto <sanshiro@uw.edu>
2025-03-26 10:54:44 -0700

    Restructure: SlowJS: added Scheduler, moved start()/update()/beat()/... from Frame to Scheduler

--- app/server/sd_version.py
+++ app/server/sd_version.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 slowdash_version = '250328 "Skykomish"'
+=======
+slowdash_version = '250326 "Skykomish"'
+>>>>>>> 141cee5 (Restructure: SlowJS: added Scheduler, moved start()/update()/beat()/... from Frame to Scheduler)
