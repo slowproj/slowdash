9e8739edb7f2f27fa4404ab8534c3a3d633c9892
Sanshiro Enomoto <sanshiro@uw.edu>
2025-04-18 17:27:07 -0700

    Applications/SlowDrip: made FirstMeshDirect working (still needs code cleaning)

--- Applications/SlowDrip/FirstMeshDirect/slowdash.d/config/slowtask-DriplineMesh.py
+++ Applications/SlowDrip/FirstMeshDirect/slowdash.d/config/slowtask-DriplineMesh.py
@@ -1,6 +1,6 @@
 
 
-import asyncio, json, re, datetime, time, socket, getpass, logging
+import asyncio, json, re, uuid, datetime, time, socket, getpass, logging
 import aio_pika
 
 from slowpy.control import ControlSystem, ControlNode
@@ -8,6 +8,7 @@ ctrl = ControlSystem()
 
 endpoint_values = {}
 heartbeat_values = {}
+replies = []
 
 
 import slowlette
@@ -30,11 +31,18 @@ def html_EndpointSet(request:slowlette.Request):
     |   <table>
     |     <tr><td>Endpoint</td><td><input name="endpoint" list="dripline_endpoints" style="width:32em"></td></tr>
     |     <tr><td>Value/Command</td><td><input name="value" style="width:32em"></td></tr>
+    |     <tr><td>Specifier</td><td><input name="specifier" style="width:32em"></td></tr>
+    |     <tr><td>Lockout Key</td><td><input name="lockout_key" style="width:32em"></td></tr>
     |     <tr><td></td><td>
+    |       <input type="submit" name="get_endpoint" value="Get Value">
     |       <input type="submit" name="set_endpoint" value="Set Value">
     |       <input type="submit" name="cmd_endpoint" value="Send Command">
     |     </td></tr>
     |   </table>
+    |   <!--
+    |   <span style="margin-left:1rem">Replies</span>
+    |   <div style="width:80%;height:8rem;margin-left:2rem;border:thin solid gray"></div>
+    |   -->
     | </form>
     '''
     html = re.sub('^[ ]*\\|', '', html, flags=re.MULTILINE)
@@ -92,7 +100,15 @@ class HeartbeatsNode(ControlNode):
             
         return {
             'columns': [ 'Name', 'Timestamp' ],
-            'table': table
+            'table': table,
+        }
+
+    
+class RepliesNode(ControlNode):
+    def get(self):
+        return {
+            'columns': ['Timestamp', 'Endpoint', 'Operation', 'Reply'],
+            'table': replies,
         }
 
     
@@ -100,24 +116,31 @@ def _export():
     return [
         ('CurrentEndpointValues', EndpointsNode()),
         ('HeartBeats', HeartbeatsNode()),
+        ('Replies', RepliesNode()),
     ]
 
 
 
-def _process_command(doc):
-    if doc.get('set_endpoint', False):
+async def _process_command(doc):
+    if doc.get('get_endpoint', False):
         endpoint = doc.get('endpoint', None)
-        value = doc.get('endpoint', None)
+        if (endpoint is None):
+            return False
+        await send_get_request(endpoint)
+        
+    elif doc.get('set_endpoint', False):
+        endpoint = doc.get('endpoint', None)
+        value = doc.get('value', None)
         if (endpoint is None) or (value is None):
             return False
-        send_set_request(endpoint, value)
+        await send_set_request(endpoint, value)
         
     elif doc.get('cmd_endpoint', False):
         endpoint = doc.get('endpoint', None)
-        value = doc.get('endpoint', None)
+        value = doc.get('value', None)
         if (endpoint is None) or (value is None):
             return False
-        send_cmd_request(endpoint, value)
+        await send_cmd_request(endpoint, value)
         
     else:
         return None
@@ -127,33 +150,33 @@ def _process_command(doc):
 
 
 
-connection, channel, queue, reply_queue = None, None, None, None
-
+connection, channel = None, None
+alerts_exchange, requests_exchange = None, None
+queue, reply_queue = None, None
         
 async def _initialize(params):
-    global connection, channel, queue
+    global connection, channel, alerts_exchange, requests_exchange, queue, reply_queue
     connection = await aio_pika.connect_robust('amqp://dripline:dripline@localhost')
     channel = await connection.channel()
     
     alerts_exchange = await channel.declare_exchange('alerts', aio_pika.ExchangeType.TOPIC)
-    requests_exchange = await channel.declare_exchange('requests', aio_pika.ExchangeType.TOPIC)
     queue = await channel.declare_queue(name='slowdash_sub', exclusive=True)
-    reply_queue = await channel.declare_queue(name='slowdash', exclusive=True)
     await queue.bind(alerts_exchange, routing_key='sensor_value.*')
     await queue.bind(alerts_exchange, routing_key='heartbeat.*')
-    await reply_queue.bind(requests_exchange, routing_key='slowdash')
+    
+    requests_exchange = await channel.declare_exchange('requests', aio_pika.ExchangeType.TOPIC)
+    reply_queue = await channel.declare_queue(name='', exclusive=True)
+    await reply_queue.bind(requests_exchange, routing_key=reply_queue.name)
 
 
 async def _finalize():
-    global connection, channel, queue
+    global connection
     if connection:
-        await connectin.close()
+        await connection.close()
         connection = None
     
         
 async def _run():
-    global connection, channel, queue
-
     while not ctrl.is_stop_requested():
         try:
             message = await queue.get()
@@ -165,23 +188,39 @@ async def _run():
 
             
 
-def send_set_request(endpoint, value):
+async def send_get_request(endpoint, **kwargs):
+    return await send_request(1, routing_key=endpoint, body='{}', **kwargs)
+    
+async def send_set_request(endpoint, value, **kwargs):
+    return await send_request(0, routing_key=endpoint, body='{"values":[%s]}'%str(value), **kwargs)
+
+async def send_cmd_request(endpoint, value, **kwargs):
+    return await send_request(9, routing_jey=endpoint, body=value, **kwargs)
+    
+
+async def send_request(operation, routing_key, body, *, specifier=None, lockout_key=None):
+    if specifier is None:
+        specifier = ''
+    if lockout_key is None:
+        lockout_key = '00000000-0000-0000-0000-000000000000'
+        
+    request_time = time.time()
     message_id = f'{uuid.uuid4()}/0/1'
     correlation_id = f'{uuid.uuid4()}'
-    channel.basic_publish(
-        routing_key=endpoint,
-        exchange='requests',
-        properties=pika.BasicProperties(
+    timestamp = datetime.datetime.fromtimestamp(request_time, tz=datetime.timezone.utc).isoformat().replace('+00:00', 'Z')
+    
+    await requests_exchange.publish(
+        aio_pika.Message(
+            reply_to = reply_queue.name,
+            correlation_id = correlation_id,
             content_encoding = 'application/json',
             message_id = message_id,
-            correlation_id = correlation_id,
-            reply_to = reply_queue.name,
             headers = {
-                'message_operation': 0,  # 0: Set, 1: Get, 9: Command
+                'message_operation': operation,  # 0: Set, 1: Get, 9: Command
                 'message_type': 3,  # 2: Reply, 3: Request, 4: Alert
-                'lockout_key': '00000000-0000-0000-0000-000000000000',
-                'specifier': '',
-                'timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat().replace('+00:00', 'Z'),
+                'lockout_key': lockout_key,
+                'specifier': specifier,
+                'timestamp': timestamp,
                 'sender_info': {
                     'exe': __file__,
                     'hostname': socket.gethostname(),
@@ -193,33 +232,30 @@ def send_set_request(endpoint, value):
                         }
                     }
                 }
-            }
+            },
+            body = body.encode()
         ),
-        body = ('{"values":[%s]}' % str(value)).encode()
+        routing_key=routing_key
     )
 
-    start_time = time.time()
+    date = datetime.datetime.fromtimestamp(request_time).strftime('%y-%m-%d %H:%M:%S')
+    op_name = {'0':'SET', '1':'GET', '9':'CMD'}.get(str(operation), 'UNKNOWN')
     while True:
         try:
             message = await reply_queue.get()
             async with message.process():
-                print("REPLY RECEIVED")
-                break
+                reply = message.body.decode()
+                replies.append([date, routing_key, f'{op_name}: {body}', reply])
+                return reply
         except aio_pika.exceptions.QueueEmpty:
             await asyncio.sleep(0.5)
-        if time.time() - start_time > 5:
-            print("REPLY TIMEOUT")
-            break
+        if time.time() - request_time > 5:
+            replies.append([date, routing_key, f'{op_name}: {body}', '**TIMEOUT**'])
+            return None
 
             
     
-def send_cmd_request(endpoint, value):
-    pass
-    
-
 def on_message(message):
-    global endpoint_values, heartbeat_values
-    
     name = message.routing_key
     record = {
         'timestamp': message.headers.get("timestamp", None),
